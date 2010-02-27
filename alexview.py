0f865cc199687726abd7f4d3b299c9ad7ad4cc7d
Matthew Brett <matthew.brett@gmail.com>
2010-02-26 17:55:50 -0800

    Try to remove * imports

--- alexview.py
+++ alexview.py
@@ -15,7 +15,7 @@ def load_nii(filename):
     return dict(imarray=image,header=header,NiftImage=nifti_image)
 
 ## Find and load the image file ##
-imfilename = "/Users/alexanderhuth/programming/niiview/anat.nii" ## You should replace this with whatever file you want to view
+imfilename = "/home/mb312/tmp/anat.nii" ## You should replace this with whatever file you want to view
 baseim = load_nii(imfilename)
 image = baseim["imarray"]
 tlen, xlen, ylen, zlen = image.shape
@@ -28,27 +28,29 @@ print "Min: %d, Max: %d" % (minval,maxval)
 scaled_image = (np.clip((image.astype(float)-minval) / (maxval-minval),0,1.0)*255).astype(np.uint8).copy()
 
 ## Initialize OpenGL ##
-from OpenGL.GL import *
+import OpenGL.GL as gl
 import pygame
-from pygame.locals import *
+import pygame.locals as pgl
 res = (700,700) ## Display screen size
 
-#texture0_arb = int(GL_TEXTURE0_ARB)
-texture0 = int(GL_TEXTURE0) ## Store integer pointer to first texture
+#texture0_arb = int(gl.GL_TEXTURE0_ARB)
+texture0 = int(gl.GL_TEXTURE0) ## Store integer pointer to first texture
 
-pygame.init() ## Initialize pygame display
-surface = pygame.display.set_mode(res, OPENGL|DOUBLEBUF) ## Turns on OpenGL and double buffering in pygame
+# Initialize pygame display
+pygame.init()
+# Turns on OpenGL and double buffering in pygame
+surface = pygame.display.set_mode(res, pgl.OPENGL | pgl.DOUBLEBUF) 
 
-glEnable(GL_TEXTURE_3D) ## Enable 3D textures
-glShadeModel(GL_SMOOTH) ## Interpolated shading
+gl.glEnable(gl.GL_TEXTURE_3D) ## Enable 3D textures
+gl.glShadeModel(gl.GL_SMOOTH) ## Interpolated shading
 
-glMatrixMode(GL_MODELVIEW) ## We're now modifying the modelview matrix
-glLoadIdentity() ## Load an identity matrix into the modelview
+gl.glMatrixMode(gl.GL_MODELVIEW) ## We're now modifying the modelview matrix
+gl.glLoadIdentity() ## Load an identity matrix into the modelview
 
-glClearColor(0.0, 0.0, 0.0, 1.0) ## Set the clear color to black
+gl.glClearColor(0.0, 0.0, 0.0, 1.0) ## Set the clear color to black
 
 ## This chunk of code came from somewhere on the internet.  It loads OpenGL extensions.
-from ctypes import *
+from ctypes import cdll
 try:
     ## For OpenGL-ctypes
     from OpenGL import platform
@@ -69,82 +71,82 @@ except ImportError:
 
 def setup_texture(volume):
     ## Set multitexturing number ##
-    #gl.glActiveTextureARB(texture0_arb+volume)
+    #gl.gl.glActiveTextureARB(texture0_arb+volume)
     
     ## Setup texturing parameters ##
-    glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexParameterf(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP)
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP)
+    gl.glTexParameterf(gl.GL_TEXTURE_3D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    gl.glTexParameterf(gl.GL_TEXTURE_3D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+    gl.glTexParameteri(gl.GL_TEXTURE_3D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP)
+    gl.glTexParameteri(gl.GL_TEXTURE_3D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP)
+    gl.glTexParameteri(gl.GL_TEXTURE_3D, gl.GL_TEXTURE_WRAP_R, gl.GL_CLAMP)
 
     ## Setup texture coordinate generation parameters ##
-    glEnable(GL_TEXTURE_GEN_S)
-    glEnable(GL_TEXTURE_GEN_T)
-    glEnable(GL_TEXTURE_GEN_R)
+    gl.glEnable(gl.GL_TEXTURE_GEN_S)
+    gl.glEnable(gl.GL_TEXTURE_GEN_T)
+    gl.glEnable(gl.GL_TEXTURE_GEN_R)
 
     ## Texture coordinates will be generated in eye-centered coordinates ##
-    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
-    glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
-    glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
+    gl.glTexGeni(gl.GL_S, gl.GL_TEXTURE_GEN_MODE, gl.GL_EYE_LINEAR)
+    gl.glTexGeni(gl.GL_T, gl.GL_TEXTURE_GEN_MODE, gl.GL_EYE_LINEAR)
+    gl.glTexGeni(gl.GL_R, gl.GL_TEXTURE_GEN_MODE, gl.GL_EYE_LINEAR)
 
     ## Object coordinates will be translated 1:1 to texture coordinates ##
-    glTexGenfv(GL_S, GL_EYE_PLANE, [1,0,0,0])
-    glTexGenfv(GL_T, GL_EYE_PLANE, [0,1,0,0])
-    glTexGenfv(GL_R, GL_EYE_PLANE, [0,0,1,0])
+    gl.glTexGenfv(gl.GL_S, gl.GL_EYE_PLANE, [1,0,0,0])
+    gl.glTexGenfv(gl.GL_T, gl.GL_EYE_PLANE, [0,1,0,0])
+    gl.glTexGenfv(gl.GL_R, gl.GL_EYE_PLANE, [0,0,1,0])
 
 def draw_all_vols(v):
     """Draw a unit xy plane at depth v.
     """
-    texmat = glGetDouble(GL_TEXTURE_MATRIX) ## Get texture xform matrix from OpenGL
+    texmat = gl.glGetDouble(gl.GL_TEXTURE_MATRIX) ## Get texture xform matrix from OpenGL
     
-    #gl.glActiveTextureARB(texture0+0)
-    gl.glActiveTextureARB(GL_TEXTURE0_ARB)
-    glEnable(GL_TEXTURE_3D)
-    glBindTexture(GL_TEXTURE_3D, int(volumes[0])) ## Binds the loaded texture as the 3D texture
+    #gl.gl.glActiveTextureARB(texture0+0)
+    gl.gl.glActiveTextureARB(gl.GL_TEXTURE0_ARB)
+    gl.glEnable(gl.GL_TEXTURE_3D)
+    gl.glBindTexture(gl.GL_TEXTURE_3D, int(volumes[0])) ## Binds the loaded texture as the 3D texture
     setup_texture(volumes[0]) ## Run texture setup routine
     
-    glMatrixMode(GL_TEXTURE) ## We're now modifying the texture xform matrix
-    glLoadMatrixd(texmat) ## And we're loading in the matrix we saved above.  Not sure why I did this.
+    gl.glMatrixMode(gl.GL_TEXTURE) ## We're now modifying the texture xform matrix
+    gl.glLoadMatrixd(texmat) ## And we're loading in the matrix we saved above.  Not sure why I did this.
     
-    #gl.glActiveTextureARB(GL_TEXTURE1_ARB)
-    #glEnable(GL_TEXTURE_3D)
-    #glBindTexture(GL_TEXTURE_3D, int(volumes[1]))
+    #gl.gl.glActiveTextureARB(gl.GL_TEXTURE1_ARB)
+    #gl.glEnable(gl.GL_TEXTURE_3D)
+    #gl.glBindTexture(gl.GL_TEXTURE_3D, int(volumes[1]))
     #setup_texture(volumes[1])
     #
-    #glMatrixMode(GL_TEXTURE)
-    #glLoadMatrixd(texmat)
+    #gl.glMatrixMode(gl.GL_TEXTURE)
+    #gl.glLoadMatrixd(texmat)
     
-    #glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_ADD)
+    #gl.glTexEnvi(gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_ADD)
     
     draw_view(v,0) ## Actually draw the view
     
-    gl.glActiveTextureARB(GL_TEXTURE0_ARB) ## Set the active texture back to the 0'th
-    glDisable(GL_TEXTURE_3D) ## Disable 3D texturing.
+    gl.gl.glActiveTextureARB(gl.GL_TEXTURE0_ARB) ## Set the active texture back to the 0'th
+    gl.glDisable(gl.GL_TEXTURE_3D) ## Disable 3D texturing.
     
-    #gl.glActiveTexture(GL_TEXTURE1_ARB)
-    #glDisable(GL_TEXTURE_3D)
+    #gl.gl.glActiveTexture(gl.GL_TEXTURE1_ARB)
+    #gl.glDisable(gl.GL_TEXTURE_3D)
     
     #for vol in [0]:#range(len(volumes)):
     #   draw_view(v,vol)
 
 def draw_view(v,volume):
     """ Draw a unit xy plane at depth v """
-    #glBindTexture(GL_TEXTURE_3D,int(volumes[volume]))
-    orig_color = glGetFloatv(GL_CURRENT_COLOR) ## Save current color so we can revert to it after
+    #gl.glBindTexture(gl.GL_TEXTURE_3D,int(volumes[volume]))
+    orig_color = gl.glGetFloatv(gl.GL_CURRENT_COLOR) ## Save current color so we can revert to it after
     if volume_colors[volume]:
-        glColor(volume_colors[volume])
+        gl.glColor(volume_colors[volume])
     
-    #glEnable(GL_TEXTURE_3D)
-    glBegin(GL_QUADS)
-    glVertex3f(0,0,v)
-    glVertex3f(1,0,v)
-    glVertex3f(1,1,v)
-    glVertex3f(0,1,v)
-    glEnd()
-    #glDisable(GL_TEXTURE_3D)
+    #gl.glEnable(gl.GL_TEXTURE_3D)
+    gl.glBegin(gl.GL_QUADS)
+    gl.glVertex3f(0,0,v)
+    gl.glVertex3f(1,0,v)
+    gl.glVertex3f(1,1,v)
+    gl.glVertex3f(0,1,v)
+    gl.glEnd()
+    #gl.glDisable(gl.GL_TEXTURE_3D)
     
-    #glColor(orig_color)
+    #gl.glColor(orig_color)
 
 
 def draw_cursor_2d(x,y,color=[0,1,0]):
@@ -153,17 +155,17 @@ def draw_cursor_2d(x,y,color=[0,1,0]):
 
 def draw_line(x0,y0,x1,y1,color):
     # Store previous color, set current color
-    orig_color = glGetFloatv(GL_CURRENT_COLOR)
-    glColor(color)
+    orig_color = gl.glGetFloatv(gl.GL_CURRENT_COLOR)
+    gl.glColor(color)
     
     # Draw line
-    glBegin(GL_LINES)
-    glVertex2f(x0,y0)
-    glVertex2f(x1,y1)
-    glEnd()
+    gl.glBegin(gl.GL_LINES)
+    gl.glVertex2f(x0,y0)
+    gl.glVertex2f(x1,y1)
+    gl.glEnd()
     
     # Reset color to original
-    glColor(orig_color)
+    gl.glColor(orig_color)
 
 def display_to_cursor(pos,reso,curs):
     """ Translate a point in screen coordinates (pos) to a position in (x,y,z) volume coordinates.
@@ -193,13 +195,13 @@ def gen_volume_display_list(n_slices=200,min_dist=0.):
     """
     max_dist = np.sqrt(3)
     
-    volume_list = glGenLists(1)
-    glNewList(volume_list,GL_COMPILE_AND_EXECUTE)
+    volume_list = gl.glGenLists(1)
+    gl.glNewList(volume_list,gl.GL_COMPILE_AND_EXECUTE)
     
     ## Draw slice planes from back to front ##
     [draw_all_vols(z) for z in np.linspace(max_dist,min_dist,n_slices)]
     
-    glEndList()
+    gl.glEndList()
     
     return volume_list
 
@@ -210,23 +212,23 @@ bigtex = np.zeros((maxlen,maxlen,maxlen)).astype(np.uint8)
 bigtex[(maxlen-xlen)/2:(maxlen+xlen)/2,(maxlen-ylen)/2:(maxlen+ylen)/2,(maxlen-zlen)/2:(maxlen+zlen)/2] = tex
 
 ## Load texture into GPU ##
-volumes = [glGenTextures(1)]
+volumes = [gl.glGenTextures(1)]
 volume_colors = [None,None]#[0.8,0.,0.]]
 
-#gl.glActiveTexture(texture0_arb+0)
-#glEnable(GL_TEXTURE_3D)
-glBindTexture(GL_TEXTURE_3D, int(volumes[0]))
-gl.glTexImage3D(GL_TEXTURE_3D, 0, 1, maxlen, maxlen, maxlen, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, bigtex.ctypes.data)
+#gl.gl.glActiveTexture(texture0_arb+0)
+#gl.glEnable(gl.GL_TEXTURE_3D)
+gl.glBindTexture(gl.GL_TEXTURE_3D, int(volumes[0]))
+gl.gl.glTexImage3D(gl.GL_TEXTURE_3D, 0, 1, maxlen, maxlen, maxlen, 0, gl.GL_LUMINANCE, gl.GL_UNSIGNED_BYTE, bigtex.ctypes.data)
 #setup_texture(0)
 
 ## Set up orthographic projection matrix ##
-glMatrixMode(GL_PROJECTION)
-glLoadIdentity()
-glOrtho(0,1,0,1,-1,1)
+gl.glMatrixMode(gl.GL_PROJECTION)
+gl.glLoadIdentity()
+gl.glOrtho(0,1,0,1,-1,1)
 
 ## Turn on blending, make transparency proportional to luminance ##
-glBlendFunc(GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR)
-glEnable(GL_BLEND)
+gl.glBlendFunc(gl.GL_SRC_COLOR, gl.GL_ONE_MINUS_SRC_COLOR)
+gl.glEnable(gl.GL_BLEND)
 
 ## Pre-compile volume rendering commands into a display list ##
 volume_dlist = 0
@@ -289,62 +291,62 @@ while not do_exit:
     slice_depth = min( max(slice_depth+dslice_depth,0.), 1.)
     
     ## Clear display ##
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
     ## Draw sagittal view in upper-left corner ##
-    glViewport(0, res[1]//2, res[0]//2, res[1]//2)
-    glMatrixMode(GL_TEXTURE)
-    glLoadIdentity()
-    glTranslate(0.5,0.5,0.5)
-    glRotate(90,0,0,1)
-    glTranslate(-0.5,-0.5,-0.5)
+    gl.glViewport(0, res[1]//2, res[0]//2, res[1]//2)
+    gl.glMatrixMode(gl.GL_TEXTURE)
+    gl.glLoadIdentity()
+    gl.glTranslate(0.5,0.5,0.5)
+    gl.glRotate(90,0,0,1)
+    gl.glTranslate(-0.5,-0.5,-0.5)
     draw_all_vols(cursor[0])
     #draw_view(cursor[0],0)
     draw_cursor_2d(cursor[1],cursor[2])
 
     ## Draw axial view in lower-left corner ##
-    glViewport(0, 0, res[0]//2, res[1]//2)
-    glMatrixMode(GL_TEXTURE)
-    glLoadIdentity()
-    glTranslate(0.5,0.5,0.5)
-    glRotate(90,0,1,0)
-    glRotate(90,0,0,1)
-    glTranslate(-0.5,-0.5,-0.5)
+    gl.glViewport(0, 0, res[0]//2, res[1]//2)
+    gl.glMatrixMode(gl.GL_TEXTURE)
+    gl.glLoadIdentity()
+    gl.glTranslate(0.5,0.5,0.5)
+    gl.glRotate(90,0,1,0)
+    gl.glRotate(90,0,0,1)
+    gl.glTranslate(-0.5,-0.5,-0.5)
     draw_all_vols(cursor[2])
     draw_cursor_2d(cursor[1],cursor[0])
 
     ## Draw coronal view in upper-right corner ##
-    glViewport(res[0]//2, res[1]//2, res[0]//2, res[1]//2)
-    glMatrixMode(GL_TEXTURE)
-    glLoadIdentity()
-    glTranslate(0.5,0.5,0.5)
-    glRotate(90,0,0,1)
-    glRotate(90,0,1,0)
-    glTranslate(-0.5,-0.5,-0.5)
+    gl.glViewport(res[0]//2, res[1]//2, res[0]//2, res[1]//2)
+    gl.glMatrixMode(gl.GL_TEXTURE)
+    gl.glLoadIdentity()
+    gl.glTranslate(0.5,0.5,0.5)
+    gl.glRotate(90,0,0,1)
+    gl.glRotate(90,0,1,0)
+    gl.glTranslate(-0.5,-0.5,-0.5)
     draw_all_vols(cursor[1])
     draw_cursor_2d(cursor[0],cursor[2])
 
     ## Draw slice or volume view in lower-right corner ##
-    glViewport(res[0]//2,0,res[0]//2,res[1]//2)
-    glMatrixMode(GL_TEXTURE)
-    glLoadIdentity()
-    glTranslate(0.5,0.5,0.5)
-    glRotate(90,0,0,1)
-    glRotate(phi,0,1,0)
-    glRotate(rho,1,0,0)
-    glTranslate(-0.5,-0.5,-0.5)
+    gl.glViewport(res[0]//2,0,res[0]//2,res[1]//2)
+    gl.glMatrixMode(gl.GL_TEXTURE)
+    gl.glLoadIdentity()
+    gl.glTranslate(0.5,0.5,0.5)
+    gl.glRotate(90,0,0,1)
+    gl.glRotate(phi,0,1,0)
+    gl.glRotate(rho,1,0,0)
+    gl.glTranslate(-0.5,-0.5,-0.5)
 
     if volume_view:
-        #glBlendFunc(GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR)
-        #glBlendFunc(GL_ONE, GL_ONE)
-        #gl.glBlendEquationEXT(32776)
-        #glEnable(GL_BLEND)
+        #gl.glBlendFunc(gl.GL_SRC_COLOR, gl.GL_ONE_MINUS_SRC_COLOR)
+        #gl.glBlendFunc(gl.GL_ONE, gl.GL_ONE)
+        #gl.gl.glBlendEquationEXT(32776)
+        #gl.glEnable(gl.GL_BLEND)
         volume_dlist = gen_volume_display_list(200,slice_depth)
         #if volume_redraw:
         #    volume_dlist = gen_volume_display_list(200,slice_depth)
         #else:
-        #    glCallList(volume_dlist)
-        #glDisable(GL_BLEND)
+        #    gl.glCallList(volume_dlist)
+        #gl.glDisable(gl.GL_BLEND)
     else:
         draw_all_vols(0.5)
     
